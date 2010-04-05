435c240f44acfb090b7ea2b62b6d91b66013d1db
Everton Ribeiro <nuxlli@gmail.com>
2010-04-05 16:19:44 -0300

    Mate == vim in Linux

--- lib/linux.bash
+++ lib/linux.bash
@@ -5,8 +5,8 @@ debug "Load linux bash configure"
 # Linux configure	
 function before_common {
 	export EDITOR="vim"
-	alias ls='ls --color=auto'
-
+	alias mate ='vim'
+	alias ls   ='ls --color=auto'
 	alias which='which 2> /dev/null'
 }
 
