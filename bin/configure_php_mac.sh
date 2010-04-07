0e97ec4662dc858891450a8fa64562d8d0265a62
Everton Ribeiro <nuxlli@gmail.com>
2010-04-06 23:26:52 -0300

    Adding open all and filter 'no github' repositories

---
+++ bin/configure_php_mac.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+./configure --prefix=/Users/eribeiro/Library/apache2 --mandir=/usr/share/man --with-mcrypt=/opt/local --infodir=/usr/share/info --disable-dependency-tracking --with-apxs2=/Users/eribeiro/Library/apache2/bin/apxs --with-ldap=/usr --with-kerberos=/usr --enable-cli --with-zlib-dir=/usr --enable-trans-sid --with-xml --with-dom --enable-exif --enable-ftp --enable-mbstring --enable-mbregex --enable-dbx --enable-sockets --with-iodbc=/usr --with-curl=/usr --with-iconv=shared,/opt/local --with-openssl --with-xmlrpc --with-xsl=/usr --with-config-file-path=/etc --with-soap --sysconfdir=/private/etc  --with-mysql=/usr/local/mysql --with-mysqli=/usr/local/mysql/bin/mysql_config --with-pdo --with-pdo-mysql=/usr/local/mysql --without-pear
+
+# --with-gd=/opt/local --enable-gd-native-ttf --with-jpeg-dir=/opt/local --with-tiff-dir=/opt/local --with-pdflib=/opt/local --with-gettext=/opt/local
